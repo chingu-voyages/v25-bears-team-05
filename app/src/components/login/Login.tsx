0475066f1f7a76f643f04f97840a30b5a7764993
lily-law <lilylaw.dev@gmail.com>
2020-11-16 16:34:07 +0000

    Add Login component placeholder

---
+++ app/src/components/login/Login.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Login.css';
+
+function Login() {
+  return (
+    <div className="Login-component">
+      <h2>Login component - TODO</h2>
+    </div>
+  );
+}
+
+export default Login;

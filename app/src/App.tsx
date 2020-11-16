a2a1b0060ac25bfa8ce2ec23e58030c181880806
lily-law <lilylaw.dev@gmail.com>
2020-11-16 14:27:31 +0000

    Add routing to App.tsx - npm i react-router-dom - npm i -D @types/react-router-dom - Remove unused files

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,25 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/network">
+          "Landing Page: TODO"
+        </Route>
+        <Route path="/profile/:userId">
+          "Profile Page: TODO"
+        </Route>
+        <Route path="/home">
+          "Home Page: TODO"
+        </Route>
+        <Route path="/signup">
+          "Signup Page: TODO"
+        </Route>
+        <Route path="/">
+          "Landing Page: TODO"
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 

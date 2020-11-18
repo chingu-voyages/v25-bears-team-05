4c029697386978d80475767b6e60430ad880d7e2
lily-law <lilylaw.dev@gmail.com>
2020-11-18 11:41:19 +0000

    Add serverAPI service

---
+++ app/src/services/serverAPI/index.ts
@@ -0,0 +1,3 @@
+import serverAPI from "./serverAPI";
+
+export default serverAPI;
---
+++ app/src/services/serverAPI/serverAPI.ts
@@ -0,0 +1,32 @@
+import axios from 'axios';
+import { getCookie } from '../../utils/cookie-parser';
+
+interface ServerAPIRequest {
+  method: "get" | "GET" | "delete" | "DELETE" | "head" | "HEAD" | "options" | "OPTIONS" | "post" | "POST" | "put" | "PUT" | "patch" | "PATCH" | "purge" | "PURGE" | "link" | "LINK" | "unlink" | "UNLINK" | undefined;
+  path: string;
+  data?: object;
+}
+
+export default async function serverAPI({method, path, data = {}}: ServerAPIRequest) {
+  const currentUserId = getCookie("userid");
+  const curerntJWT = getCookie("jwt");
+  if (!currentUserId || !curerntJWT) {
+    return {
+      status: 401,
+      statusText: "Unauthorized"
+    }
+  }
+  if (path.includes(":userid")) {
+    path = path.split(":userid").join(currentUserId);
+  }
+  const headers = {
+      Authorization: `Bearer ${curerntJWT}`,
+      "Content-Type": "application/json",
+  };
+  return await axios({
+      method,
+      url: path,
+      data,
+      headers
+  })
+}

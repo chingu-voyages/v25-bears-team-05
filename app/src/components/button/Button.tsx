0c336c6c8f138c714a2d307067b8afc8b59541f0
lily-law <lilylaw.dev@gmail.com>
2020-11-19 15:17:32 +0000

    Work in progress

--- app/src/components/button/Button.tsx
+++ app/src/components/button/Button.tsx
@@ -1,20 +1,13 @@
 import React from "react";
 import "./Button.css";
 
-function Button({
-  className,
-  onClick = () => null,
-  children,
-}: {
-  className?: string;
-  onClick?: (...args: any) => any;
-  children?: React.ReactNode;
-}) {
-  return (
-    <button className={`Button ${className}`} onClick={onClick}>
-      {children}
-    </button>
-  );
+function Button(
+  props: React.DetailedHTMLProps<
+    React.ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+  >
+) {
+  return <button {...props} className={`Button ${props.className}`} />;
 }
 
 export default Button;
--- app/src/components/login/Login.tsx
+++ app/src/components/login/Login.tsx
@@ -1,10 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
+import Input from "../input";
+import isEmailValid from "../../utils/isEmailValid";
 import "./Login.css";
+import Button from "../button";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [isEmailError, setIsEmailError] = useState(false);
+  const validateEmail = (currentEmail: string) => {
+    setIsEmailError(!isEmailValid(currentEmail));
+  };
+  const handleEmailOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.target.value ? validateEmail(e.target.value) : setIsEmailError(false);
+    setEmail(e.target.value);
+  };
+  const handleSignin = () => {
+    const errors = !isEmailValid(email) && setIsEmailError(true);
+    if (!errors) {
+      // axios
+    }
+  };
+  const [password, setPassword] = useState("");
+  const [passwordErrorMessage, setPasswordErrorMessage] = useState("");
+  const validatePassword = (currentPassword: string) => {
+    let errors = [];
+    currentPassword.length < 8 && errors.push("at least 8 characters");
+    (!currentPassword.match(/[A-Z]/) || !currentPassword.match(/[a-z]/)) &&
+      errors.push("a mix of uppercase and lowercase");
+    setPasswordErrorMessage(
+      errors.length > 0
+        ? ["Password requires ", errors.join(" and ")].join("")
+        : ""
+    );
+  };
+  const handlePasswordOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.target.value
+      ? validatePassword(e.target.value)
+      : setPasswordErrorMessage("");
+    setPassword(e.target.value);
+  };
   return (
     <div className="Login-component">
       <h2>Login component - TODO</h2>
+      <Input
+        label="Email"
+        id="loginEmail"
+        type="email"
+        value={email}
+        onChange={handleEmailOnChange}
+        errorMessage={isEmailError ? "Please enter a valid email" : ""}
+      />
+      <Input
+        label="Password"
+        id="loginPassword"
+        type="password"
+        value={password}
+        onChange={handlePasswordOnChange}
+        errorMessage={passwordErrorMessage}
+      />
+      <Button onClick={handleSignin} type="submit" aria-label="Sign in">
+        Sign in
+      </Button>
     </div>
   );
 }

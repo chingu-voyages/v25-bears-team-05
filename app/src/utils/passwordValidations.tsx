5e0693b403a744908ec248aebb50b0fbdff60a92
lily-law <lilylaw.dev@gmail.com>
2020-11-20 14:11:44 +0000

    Fix inverted valiation assertion

--- app/src/utils/passwordValidations.tsx
+++ app/src/utils/passwordValidations.tsx
@@ -2,7 +2,7 @@ const isPasswordTooShort = (password: string) => {
   return password.length < 8;
 };
 const doesPasswordHaveMixedCase = (password: string) => {
-  return !password.match(/[A-Z]/) || !password.match(/[a-z]/);
+  return password.match(/[A-Z]/) || password.match(/[a-z]/);
 };
 
 const isPasswordValid = (password: string) => {

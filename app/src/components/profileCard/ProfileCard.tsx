f37daf803c2b6e50a4e828e40578104bdf8cb564
Lily <46561378+lily-law@users.noreply.github.com>
2020-11-24 11:32:29 +0000

    Prevent undefined rendering in ProfileCard

--- app/src/components/profileCard/ProfileCard.tsx
+++ app/src/components/profileCard/ProfileCard.tsx
@@ -14,33 +14,40 @@ function ProfileCard({
   let title = "...";
   let info: string | React.RefAttributes<HTMLAnchorElement> = "...";
   if (profileInfo) {
-    name = `${profileInfo.firstName} ${profileInfo.lastName}`;
-    title = profileInfo.jobTitle;
+    const { firstName, lastName, jobTitle, nOfConnections } = profileInfo;
+    name = `${firstName ? firstName : ""} ${lastName ? lastName : ""}`.trim();
+    title = jobTitle;
     info = (
       <Link to="/network">{`${
-        Number.isInteger(profileInfo.nOfConnections)
-          ? profileInfo.nOfConnections
-          : "..."
+        Number.isInteger(nOfConnections) ? nOfConnections : "..."
       } Connections`}</Link>
     );
   } else if (connectionInfo) {
-    name = `${connectionInfo.firstName} ${connectionInfo.lastName}`;
-    title = connectionInfo.jobTitle;
+    const { firstName, lastName, jobTitle, dateTimeConnected } = connectionInfo;
+    name = `${firstName ? firstName : ""} ${lastName ? lastName : ""}`.trim();
+    title = jobTitle;
     info = convertDateStringToTimeAgo({
-      date: connectionInfo.dateTimeConnected,
+      date: dateTimeConnected,
     });
   } else if (threadInfo) {
-    name = `${threadInfo.firstName} ${threadInfo.lastName}`;
-    title = threadInfo.jobTitle;
+    const {
+      firstName,
+      lastName,
+      jobTitle,
+      dateTimePosted,
+      visibility,
+    } = threadInfo;
+    name = `${firstName ? firstName : ""} ${lastName ? lastName : ""}`.trim();
+    title = jobTitle;
     info = `${convertDateStringToTimeAgo({
-      date: threadInfo.dateTimePosted,
-    })} &bull; ${threadInfo.visibility}`;
+      date: dateTimePosted,
+    })} &bull; ${visibility}`;
   }
 
   return (
     <div className={`Profile-card ${className}`}>
-      <h1 className="Profile-card__name">{name.trim() || "... ..."}</h1>
-      <h2 className="Profile-card__title">{title.trim() || "..."}</h2>
+      <h1 className="Profile-card__name">{name || "... ..."}</h1>
+      <h2 className="Profile-card__title">{title || "..."}</h2>
       <p className="Profile-card__info">{info || "..."}</p>
     </div>
   );

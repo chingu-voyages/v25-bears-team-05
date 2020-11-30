2904af3a874691b890a1304be6aef64208b07dcb
lily-law <lilylaw.dev@gmail.com>
2020-11-30 14:53:12 +0000

    Add Network page header

--- app/src/pages/network/Network.tsx
+++ app/src/pages/network/Network.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import "./Network.css";
+import backIcon from "../../images/backicon.svg";
 
 function Network() {
+  const history = useHistory();
+  const handleGoBack = () => history.goBack();
   return (
     <div className="Network-page">
-      <h1>Network page - TODO</h1>
+      <header className="Network-page__top-bar">
+        <Button role="link" onClick={handleGoBack}>
+          <img className="Network-page__back-icon" src={backIcon} alt="back" />
+        </Button>
+        <h1 className="Network-page__title">Connections</h1>
+      </header>
     </div>
   );
 }

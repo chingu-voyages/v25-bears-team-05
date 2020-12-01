d47670acbb30459e3437b4568e86c00150759c57
lily-law <lilylaw.dev@gmail.com>
2020-12-01 16:04:32 +0000

    Default convertDateStringToTimeAge to empty string

--- app/src/utils/convertDateStringToTimeAgo.ts
+++ app/src/utils/convertDateStringToTimeAgo.ts
@@ -5,48 +5,49 @@ const convertDateStringToTimeAgo = ({
   date: string;
   precision?: number;
 }) => {
-  const nowDate = new Date();
-  const thenDate = new Date(date);
-  if (!Number.isInteger(thenDate.valueOf())) {
-    throw Error(`Invalid date value ${date}`);
-  }
-  let nOfStringsRequired = precision;
   let timeAgo = "";
-  const yearDiff = nowDate.getUTCFullYear() - thenDate.getUTCFullYear();
-  const monthDiff = nowDate.getUTCMonth() - thenDate.getUTCMonth();
-  const dateDiff = nowDate.getUTCDate() - thenDate.getUTCDate();
-  const hourDiff = nowDate.getUTCHours() - thenDate.getUTCHours();
-  const minuteDiff = nowDate.getUTCMinutes() - thenDate.getUTCMinutes();
-  const secondDiff = nowDate.getUTCSeconds() - thenDate.getUTCSeconds();
-  const millisecondDiff =
-    nowDate.getUTCMilliseconds() - thenDate.getUTCMilliseconds();
-  const addToTimeAgo = (number: number, baseName: string) => {
-    timeAgo += `${number} ${Math.abs(number) > 1 ? baseName + "s" : baseName} `;
-    nOfStringsRequired--;
-  };
-  if (nOfStringsRequired && yearDiff) {
-    addToTimeAgo(yearDiff, "year");
-  }
-  if (nOfStringsRequired && monthDiff) {
-    addToTimeAgo(monthDiff, "month");
-  }
-  if (nOfStringsRequired && dateDiff) {
-    addToTimeAgo(dateDiff, "day");
-  }
-  if (nOfStringsRequired && hourDiff) {
-    addToTimeAgo(hourDiff, "hour");
-  }
-  if (nOfStringsRequired && minuteDiff) {
-    addToTimeAgo(minuteDiff, "minute");
-  }
-  if (nOfStringsRequired && secondDiff) {
-    addToTimeAgo(secondDiff, "second");
-  }
-  if (nOfStringsRequired && millisecondDiff) {
-    addToTimeAgo(millisecondDiff, "millisecond");
-  }
-  if (timeAgo) {
-    timeAgo = `${timeAgo}ago`;
+  const thenDate = new Date(date);
+  if (Number.isInteger(thenDate.valueOf())) {
+    const nowDate = new Date();
+    let nOfStringsRequired = precision;
+    const yearDiff = nowDate.getUTCFullYear() - thenDate.getUTCFullYear();
+    const monthDiff = nowDate.getUTCMonth() - thenDate.getUTCMonth();
+    const dateDiff = nowDate.getUTCDate() - thenDate.getUTCDate();
+    const hourDiff = nowDate.getUTCHours() - thenDate.getUTCHours();
+    const minuteDiff = nowDate.getUTCMinutes() - thenDate.getUTCMinutes();
+    const secondDiff = nowDate.getUTCSeconds() - thenDate.getUTCSeconds();
+    const millisecondDiff =
+      nowDate.getUTCMilliseconds() - thenDate.getUTCMilliseconds();
+    const addToTimeAgo = (number: number, baseName: string) => {
+      timeAgo += `${number} ${
+        Math.abs(number) > 1 ? baseName + "s" : baseName
+      } `;
+      nOfStringsRequired--;
+    };
+    if (nOfStringsRequired && yearDiff) {
+      addToTimeAgo(yearDiff, "year");
+    }
+    if (nOfStringsRequired && monthDiff) {
+      addToTimeAgo(monthDiff, "month");
+    }
+    if (nOfStringsRequired && dateDiff) {
+      addToTimeAgo(dateDiff, "day");
+    }
+    if (nOfStringsRequired && hourDiff) {
+      addToTimeAgo(hourDiff, "hour");
+    }
+    if (nOfStringsRequired && minuteDiff) {
+      addToTimeAgo(minuteDiff, "minute");
+    }
+    if (nOfStringsRequired && secondDiff) {
+      addToTimeAgo(secondDiff, "second");
+    }
+    if (nOfStringsRequired && millisecondDiff) {
+      addToTimeAgo(millisecondDiff, "millisecond");
+    }
+    if (timeAgo) {
+      timeAgo = `${timeAgo}ago`;
+    }
   }
   return timeAgo;
 };

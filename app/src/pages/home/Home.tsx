98a5680d661461ae7d8eb18d04dfcfa48d70db50
Lily <46561378+lily-law@users.noreply.github.com>
2020-12-08 20:10:51 +0000

    Add rough implementation of PostMaker in Home page

--- app/src/pages/home/Home.tsx
+++ app/src/pages/home/Home.tsx
@@ -1,19 +1,68 @@
-import React from "react";
+import React, {useState} from "react";
 import Button from "../../components/button";
 import PostMaker from "../../components/postMaker";
 import logout from "../../services/logout";
 import "./Home.css";
+import editIcon from "../../images/editicon.svg";
+import { IPostMakerProps } from "../../components/postMaker/PostMaker.type";
+import { IThread, ThreadVisibility } from "../../services/thread/thread.type";
+import { addThread } from "../../services/thread";
+import Spinner from "../../components/spinner";
 
 function Home() {
+  const [feed, setFeed] = useState([]);
+  const [isPostMakerOpen, setIsPostMakerOpen] = useState(false);
+  const [makePostError, setMakePostError] = useState("");
+  const [inProgress, setInProgress] = useState(false);
+
+  const resetPostMaker = () => {
+    setIsPostMakerOpen(false);
+    setMakePostError("");
+  }
+  const postMakerOptions: IPostMakerProps = {
+    title: "Share",
+    placeholder: "Share your thoughts. Add photos or hashtags.",
+    onSubmit: ({content}) => {
+      setInProgress(true);
+      const onSuccess = (data: IThread) => {
+        console.log(data);
+        setInProgress(false);
+        resetPostMaker();
+      };
+      addThread({
+        data: {
+          htmlContent: content,
+          threadType: 0,
+          visibility: 0,
+          hashTags: []
+        },
+        onSuccess,
+        onError: (msg) => { setMakePostError(msg) }
+      })
+    },
+    handleCancel: () => { resetPostMaker(); },
+    errorMessage: makePostError,
+    className: "Home__post-maker",
+    fullView: true
+  }
   return (
     <div className="Home-page">
       <h1>Home page - TODO</h1>
       <Button onClick={logout} className="square">
         Temporary logout button
       </Button>
-      <PostMaker/>
+      { !isPostMakerOpen ?
+          <Button onClick={() => setIsPostMakerOpen(true)} className="Post-maker__start">
+              <img src={editIcon} alt="" />
+              <h1>Share your thoughts or photos</h1>
+          </Button>
+        :
+          <PostMaker {...postMakerOptions} />
+      }
+      { inProgress && <Spinner className="Home__spinner" /> }
     </div>
   );
 }
 
 export default Home;
+

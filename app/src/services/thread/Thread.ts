f7a50be1f823e39c4367449013e694941956fefb
Lily <46561378+lily-law@users.noreply.github.com>
2020-12-08 14:53:30 +0000

    Make thread service

---
+++ app/src/services/thread/Thread.ts
@@ -0,0 +1,33 @@
+import { INewThreadData, IThread } from "./thread.type";
+import axios from "axios";
+
+const addThread = async ({
+    data,
+    onSuccess,
+    onError,
+}: {
+    data: INewThreadData;
+    onSuccess: (data: IThread) => void;
+    onError: (message: string) => void;
+}) => {
+    try {
+        const req = await axios({
+            method: "post",
+            url: `/api/threads`,
+            data,
+        });
+        if (req.status === 200) {
+            onSuccess(req.data);
+        } else {
+            onError(req.statusText);
+        }
+    } catch (error) {
+        console.error(error);
+        typeof error?.message === "string" &&
+        onError(
+            "Sorry, we're unable to add your post at this time, please try again later"
+        );
+    }
+};
+
+export default addThread;

dc8f24097887080134856725743209b309f1c561
lily-law <lilylaw.dev@gmail.com>
2020-12-09 16:45:11 +0000

    Update Home page

--- app/src/pages/home/Home.tsx
+++ app/src/pages/home/Home.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Button from "../../components/button";
 import PostMaker from "../../components/postMaker";
 import logout from "../../services/logout";
@@ -8,6 +8,7 @@ import { IPostMakerProps } from "../../components/postMaker/PostMaker.type";
 import { IThread, ThreadVisibility } from "../../services/thread/thread.type";
 import { addThread } from "../../services/thread";
 import Spinner from "../../components/spinner";
+import Post from "../../components/post";
 
 function Home() {
   const [feed, setFeed] = useState([]);
@@ -18,11 +19,11 @@ function Home() {
   const resetPostMaker = () => {
     setIsPostMakerOpen(false);
     setMakePostError("");
-  }
+  };
   const postMakerOptions: IPostMakerProps = {
     title: "Share",
     placeholder: "Share your thoughts. Add photos or hashtags.",
-    onSubmit: ({content, threadVisibility}) => {
+    onSubmit: ({ content, threadVisibility }) => {
       setInProgress(true);
       const onSuccess = (data: IThread) => {
         console.log(data);
@@ -34,35 +35,59 @@ function Home() {
           htmlContent: content,
           threadType: 0,
           visibility: threadVisibility,
-          hashTags: []
+          hashTags: [],
         },
         onSuccess,
-        onError: (msg) => { setMakePostError(msg) }
-      })
+        onError: (msg) => {
+          setMakePostError(msg);
+        },
+      });
     },
-    handleCancel: () => { resetPostMaker(); },
+    handleCancel: () => {
+      resetPostMaker();
+    },
     errorMessage: makePostError,
     className: "Home__post-maker",
-    fullView: true
-  }
+    fullView: true,
+  };
+
   return (
     <div className="Home-page">
-      <h1>Home page - TODO</h1>
-      <Button onClick={logout} className="square">
-        Temporary logout button
-      </Button>
-      { !isPostMakerOpen ?
-          <Button onClick={() => setIsPostMakerOpen(true)} className="Post-maker__start">
-              <img src={editIcon} alt="" />
-              <h1>Share your thoughts or photos</h1>
+      <div>
+        <h1>Home page - TODO</h1>
+        <Button onClick={logout} className="square">
+          Temporary logout button
+        </Button>
+      </div>
+      <div>
+        {!isPostMakerOpen ? (
+          <Button
+            onClick={() => setIsPostMakerOpen(true)}
+            className="Post-maker__start"
+          >
+            <img src={editIcon} alt="" />
+            <h1>Share your thoughts or photos</h1>
           </Button>
-        :
+        ) : (
           <PostMaker {...postMakerOptions} />
-      }
-      { inProgress && <Spinner className="Home__spinner" /> }
+        )}
+      </div>
+      <div className="Home-page__feed">
+        <ul className="Home-page__feed__list">
+          {/*feed.map(({threadData, profileData, referral, className}: {threadData: IThreadDataProcessed, profileData: IThreadCardInfo, referral?: ITheadReferral, className?: string}) => (
+            <ConnectionItem key={connectionData.id} {...{ connectionData }} />
+          )) */}
+        </ul>
+        {/* <Pagenator {...{ page, nextPage, active: isEndPage || connections.length > 0 }} />  */}
+      </div>
+      <nav>
+        <Button>Home</Button>
+        <Button>Post</Button>
+        <Button>My Network</Button>
+      </nav>
+      {inProgress && <Spinner className="Home__spinner" />}
     </div>
   );
 }
 
 export default Home;
-

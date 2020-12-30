96a81328d557c27ddded5cb5ad18de2d708022fe
Lily <46561378+lily-law@users.noreply.github.com>
2020-12-30 23:37:24 +0000

    Add comment type to ProfileCard

--- app/src/components/profileCard/profileCard.type.ts
+++ app/src/components/profileCard/profileCard.type.ts
@@ -18,7 +18,7 @@ export interface ICardInfo extends IBasicCardInfo {
 }
 
 export default interface IProfileCard {
-  type: "profile" | "connection" | "thread";
+  type: "profile" | "connection" | "thread" | "comment";
   data?: ICardInfo;
   userId?: string
   className?: string;

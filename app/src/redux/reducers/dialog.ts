7ab45312462682a568f793b0fa212a08e3938a2a
lily-law <lilylaw.dev@gmail.com>
2021-01-11 23:16:12 +0000

    Add a redux store for a dialog component

---
+++ app/src/redux/reducers/dialog.ts
@@ -0,0 +1,46 @@
+import { ADD_ERROR_MESSAGE, ADD_SUCCESS_MESSAGE, ADD_INFO_MESSAGE, SET_LOADING, CLEAR_DIALOG } from "../actionTypes";
+
+type TMessage = "error" | "success" | "info";
+const initialState: {
+  loading: boolean,
+  log: Array<{type: TMessage, message: string}>
+} = {
+  loading: false,
+  log: []
+};
+
+export default function dialog(state: any = initialState, action: any) {
+    const handleAddMessage = (type: TMessage) => {
+        const newLog = [{type, message: action.payload.message}, ...state.log];
+        return {
+            ...state,
+            log: newLog
+        }
+    };
+    switch (action.type) {
+        case ADD_ERROR_MESSAGE: {
+            return handleAddMessage("error");
+        }
+        case ADD_SUCCESS_MESSAGE: {
+            return handleAddMessage("success");
+        }
+        case ADD_INFO_MESSAGE: {
+            return handleAddMessage("info");
+        }
+        case SET_LOADING: {
+            return {
+                ...state,
+                loading: action.payload.loading
+            }
+        }
+        case CLEAR_DIALOG: {
+            return {
+                ...state,
+                loading: false,
+                log: []
+            }
+        }
+        default:
+        return state;
+    }
+}
--- app/src/redux/reducers/index.ts
+++ app/src/redux/reducers/index.ts
@@ -1,4 +1,5 @@
 import { combineReducers } from "redux";
+import dialog from "./dialog";
 import users from "./users";
 
-export default combineReducers({ users });
+export default combineReducers({ users, dialog });

b18aaa58520f52c16dca566c6fd0502a2f2747be
lily-law <lilylaw.dev@gmail.com>
2021-01-11 23:17:26 +0000

    Add method for calling services from reducers

---
+++ app/src/redux/handleServiceRequest.ts
@@ -0,0 +1,26 @@
+import { addErrorMessage, addSuccessMessage, setLoading } from "./actions/dialog";
+import store from "./store";
+
+interface IHandleServiceRequestProps {
+    requestFunction: any;
+    requestProps: { [keyof: string]: any; }
+}
+
+const handleServiceRequest = ({requestFunction, requestProps}: IHandleServiceRequestProps) => {
+    const onSuccess = (message: string) => {
+        store.dispatch(setLoading(false));
+        store.dispatch(addSuccessMessage(message));
+    };
+    const onError = (message: string) => {
+        store.dispatch(setLoading(false));
+        store.dispatch(addErrorMessage(message));
+    };
+    store.dispatch(setLoading(true));
+    requestFunction({ 
+        ...requestProps,
+        onSuccess,
+        onError
+    });
+}
+
+export default handleServiceRequest;

5a88a41c6c99ffa81670c78297bdac018d8ce120
lily-law <lilylaw.dev@gmail.com>
2021-02-01 17:12:17 +0000

    Change where envs are loaded

---
+++ dotenv/config.js
@@ -0,0 +1,7 @@
+const result = dotenv.config()
+
+if (result.error) {
+  throw result.error
+}
+
+console.log(result.parsed)
--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-require("dotenv").config();
 const express = require("express");
 const app = require("express")();
 const http = require("http").createServer(app);

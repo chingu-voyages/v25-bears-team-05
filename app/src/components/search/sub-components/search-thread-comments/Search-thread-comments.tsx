942ac1af0ea53c1df0edc24252069a7fa788a135
David Eastmond <davebuck1981@gmail.com>
2021-02-12 13:00:20 -0500

    SearchThreadComment css and starting tsx

--- app/src/components/search/sub-components/search-thread-comments/Search-thread-comments.tsx
+++ app/src/components/search/sub-components/search-thread-comments/Search-thread-comments.tsx
@@ -1,8 +1,14 @@
 import React from "react";
+import Avatar from "../../../avatar";
 import "./Search-thread-comments.css";
 
-function SearchThreadComment ({className}: {className?: string}) {
-
+function SearchThreadComment ({ className, queryString }: 
+  { queryString: string, className?: string}) {
+  return (
+    <div className={`${className || ""} SearchThreadComment__main`}>
+      <Avatar />
+    </div>
+  )
 }
 
 export default SearchThreadComment;

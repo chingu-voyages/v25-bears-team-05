d78b73a3d259de171539582aa2b8d7c09effbfca
David Eastmond <davebuck1981@gmail.com>
2021-04-02 07:41:12 -0400

    Created component and route

---
+++ app/src/pages/password-recovery/password-recovery.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./password-recovery.css";
+function PasswordRecovery() {
+  return (
+    <div className="Password-recovery-request-page">password recovery</div>
+  );
+}
+
+export default PasswordRecovery;

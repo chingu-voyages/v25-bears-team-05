2fc63f76526b6e460896c4d89fe3ff377c9261ef
David Eastmond <davebuck1981@gmail.com>
2021-04-24 08:07:49 -0400

    make-claim request function implemented

--- app/src/pages/password-recovery/recovery-claim/Recovery-claim.tsx
+++ app/src/pages/password-recovery/recovery-claim/Recovery-claim.tsx
@@ -1,24 +1,134 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./recovery-claim.css";
 import { useLocation } from "react-router-dom";
 import { parseIdParameter } from "../utils/parse-id-parameter";
 import assert from "assert";
+import axios from "axios";
+import Logo from "../../../components/logo";
+import Input from "../../../components/input";
+import Button from "../../../components/button";
+import getInvalidPasswordMessage from "../../../utils/getInvalidPasswordMessage";
+import { makeClaimRequest } from "../utils/make-claim-request";
 
 function RecoveryClaim() {
   const search = useLocation().search;
   const parseResult = parseIdParameter({ fullQueryString: search });
   const id = new URLSearchParams(search).get("id")?.replace(" ", "+");
   const data = new URLSearchParams(search).get("data");
+  const [validRequestState, setValidRequestState] = useState<boolean>(true);
+  const [firstPasswordEntry, setFirstPasswordEntry] = useState("");
+  const [secondPasswordEntry, setSecondPasswordEntry] = useState("");
+  const [requestErrorMessage, setRequestErrorMessage] = useState("");
+  const [hashedEmail, setHashedEmail] = useState<string>("");
+  const [requestToken, setRequestToken] = useState<string>("");
+  const [claimSuccessful, setClaimSuccessful] = useState<boolean>(false);
+  // const [errorMessages, setErrorMessages] = useState(["", "", "", "", ""]);
+  // const [isLoading, setIsLoading] = useState<boolean>(false);
 
   /* URLSearchParams replaces the '+' symbol with spaces. This assert is
     to ensure that the parseResult and the .replace(" ", "+") method call
     return identical values
   */
-  assert(id === parseResult, "id parameter query string mismatch");
+  assert(id === parseResult, "id parameter query string mismatch"); // POIJ: Need a more graceful fail
+  useEffect(() => {
+    (async () => {
+      try {
+        const res = await axios.get("/api/recover/verify", {
+          params: {
+            id: id,
+            data: data,
+            devBypass: true, // POIJ: to be removed - this is just to test that UI loads without having a valid request
+          },
+        });
+        setHashedEmail(res.data.id);
+        setRequestToken(res.data.data);
+      } catch (error) {
+        setValidRequestState(false);
+        setRequestErrorMessage(error.response.data);
+      }
+    })();
+  }, []);
+
+  const handleSubmitRequest = async () => {
+    if (hashedEmail && requestToken) {
+      if (firstPasswordEntry === secondPasswordEntry) {
+        makeClaimRequest({
+          hashedEmail: hashedEmail,
+          authToken: requestToken,
+          updatedPasswords: {
+            first_password: firstPasswordEntry,
+            second_password: secondPasswordEntry,
+          },
+          onError: (error) => setRequestErrorMessage(error.statusText),
+          onSuccess: (_result) => setClaimSuccessful(true),
+        });
+      } else {
+        setRequestErrorMessage(
+          "Please check that you've entered valid matching passwords."
+        );
+      }
+    } else {
+      setRequestErrorMessage("Request cannot be completed");
+    }
+  };
 
   return (
-    <div>
-      <p> This is the recovery claim page </p>
+    <div className="Password-recovery-claim__page">
+      <header className="Password-recovery-request-page__header-bar">
+        <Logo dark={true} />
+        <h2 className="black-header-text">Recover password</h2>
+      </header>
+      { claimSuccessful && 
+      <div className="Password-recovery-claim__input-section">
+        <Input
+          label="Enter a new password"
+          id="updatePassword1"
+          type="password"
+          value={firstPasswordEntry}
+          setValue={setFirstPasswordEntry}
+          validationMessenger={getInvalidPasswordMessage}
+          // errorMessageReturner={(message) =>
+          //   handleErrorMessageReturn(3, message)
+          // }
+          className="breakout-on-large-view"
+        />
+        <Input
+          label="Confirm new password"
+          id="updatePassword2"
+          type="password"
+          value={secondPasswordEntry}
+          setValue={setSecondPasswordEntry}
+          validationMessenger={getInvalidPasswordMessage}
+          // errorMessageReturner={(message) =>
+          //   handleErrorMessageReturn(3, message)
+          // }
+          className="breakout-on-large-view"
+        />
+      </div>
+      }
+      <div className="Password-submit-buttons">
+        <Button
+          onClick={handleSubmitRequest}
+          type="submit"
+          aria-label="Submit"
+          className="square Register__submit"
+        >
+          Submit
+        </Button>
+      </div>
+
+      <div className="Password-recovery-claim__error-messages">
+        {!validRequestState && <p> {requestErrorMessage} </p>}
+      </div>
+      {claimSuccessful && (
+        <div className="Password-recovery-claim-success">
+          <p>
+            {" "}
+            Password successfully claimed. Please log in with your new
+            credentials.
+          </p>
+        </div>
+      )}
     </div>
   );
 }
---
+++ app/src/pages/password-recovery/utils/make-claim-request.ts
@@ -0,0 +1,27 @@
+import axios from "axios";
+export async function makeClaimRequest({
+  hashedEmail,
+  authToken,
+  updatedPasswords,
+  onSuccess,
+  onError,
+}: {
+  hashedEmail: string;
+  authToken: string;
+  updatedPasswords: { first_password: string; second_password: string };
+  onSuccess: (result: any) => void;
+  onError: (error: any) => void;
+}) {
+  try {
+    const result = await axios.patch("/api/recover/claim", {
+      id: hashedEmail,
+      data: authToken,
+      first_password: updatedPasswords.first_password,
+      second_password: updatedPasswords.second_password
+    });
+    result.status === 200 && onSuccess(result);
+    onError("Some error happened during claim request");
+  } catch (exception) {
+    onError(exception);
+  }
+}

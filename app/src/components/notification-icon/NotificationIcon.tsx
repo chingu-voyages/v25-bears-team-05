ac33f246b17c4097e2c3f2aa9860e90bc6e1496f
David Eastmond <davebuck1981@gmail.com>
2021-06-07 19:49:56 -0400

    fixed CSS. Small notificationIcon component refactor

--- app/src/components/notification-icon/NotificationIcon.tsx
+++ app/src/components/notification-icon/NotificationIcon.tsx
@@ -1,15 +1,13 @@
 import React from "react";
 import { normalIcon } from "./normal-icon";
-import { alertIcon } from "./alert-icon";
 import "./notificationIcon.css";
-function NotificationIcon({
-  notificationIndicatorOn,
-}: {
-  notificationIndicatorOn: boolean;
-}) {
+function NotificationIcon({ count }: { count: number }) {
   return (
     <div className="Notification__icon">
-      {notificationIndicatorOn === true ? alertIcon : normalIcon}
+      {normalIcon}
+      {count > 0 && (
+        <span className="Notification__count">{count.toString()}</span>
+      )}
     </div>
   );
 }

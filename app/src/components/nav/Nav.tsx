5034299748d3f3e8e1b89afe95377ebbfeb31512
David Eastmond <davebuck1981@gmail.com>
2021-10-02 08:44:48 -0400

    trying some CSS tweaks

--- app/src/components/nav/Nav.tsx
+++ app/src/components/nav/Nav.tsx
@@ -87,7 +87,9 @@ function Nav({ className }: { className?: string }) {
         <span className="Nav__title">My Network</span>
       </Link>
       <OptionsMenu
-        className={`Nav__avatar-menu ${page === "profile" ? "active" : ""}`}
+        className={`Nav__avatar-menu ${
+          page === "profile" ? "active" : ""
+        } menu-absolute-position`}
         buttons={{
           "View Profile": { type: "link", linkTo: `/${userInfo?.id}/profile` },
           "Edit Profile": { type: "link", linkTo: "/me/profile" },
